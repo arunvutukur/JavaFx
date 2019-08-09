3fd93477065aae49337b80a1e483ddf41b281262
arun <arunv04@gmail.com>
2019-08-09 15:23:23 +0530

    Updated File chooser

--- src/main/java/sample/controller/Controller.java
+++ src/main/java/sample/controller/Controller.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.util.Random;
 
 public class Controller {
@@ -15,10 +19,53 @@ public class Controller {
     @FXML
     private Button ok;
 
+    @FXML
+    private MenuItem menuitem;
+
+    @FXML
+    private Menu menu;
+
+    @FXML
+    private Label filestatus;
+
+    private Boolean fileselected;
+
     public void generateRandom(ActionEvent event){
         Random rand = new Random();
         int myrand = rand.nextInt(50)+1;
         System.out.println(Integer.toString(myrand));
         message.setText(Integer.toString(myrand));
+    }
+
+    public void getFile(ActionEvent event){
+
+        FileChooser fileChooser =new FileChooser();
+        File selectedFile= fileChooser.showOpenDialog(null);
+        try {
+            if (selectedFile != null) {
+                System.out.println(selectedFile);
+                filestatus.setText(selectedFile.toString());
+                fileselected = true;
+            } else {
+                System.out.println("ERROR EXCEPTION");
+            }
+            System.out.println(menu);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeData(ActionEvent event){
+        if(fileselected==true){
+
+            System.out.println("Executing the below operation");
+
+
+        }else{
+            System.out.println("Cant execute this operation");
+
+        }
+
+
     }
 }
